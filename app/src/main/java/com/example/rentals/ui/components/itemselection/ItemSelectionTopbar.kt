da28902574cafa71ca8b99cafe398ad7fa5cdc3d
avinash <avinashnanda008@gmail.com>
2025-07-04 13:35:37 +0530

    added: topbar

--- app/src/main/java/com/example/rentals/ui/components/itemselection/ItemSelectionTopbar.kt
+++ app/src/main/java/com/example/rentals/ui/components/itemselection/ItemSelectionTopbar.kt
@@ -1,23 +1,30 @@
 package com.example.rentals.ui.components.itemselection
 
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ItemSelectionTopBar() {
-    TopAppBar(title = {
-        Text(text = "Search")
-    }, navigationIcon = {
-        IconButton(onClick = {}) {
-            Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
-        }
-    }, windowInsets = WindowInsets(top = 0))
+    Surface(modifier = Modifier.fillMaxWidth(), shadowElevation = 4.dp) {
+        TopAppBar(title = {
+            Text(text = "Search")
+        }, navigationIcon = {
+            IconButton(onClick = {
+            }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+            }
+        }, windowInsets = WindowInsets(top = 0))
+    }
 }
--- app/src/main/java/com/example/rentals/ui/screens/ItemSelectionScreen.kt
+++ app/src/main/java/com/example/rentals/ui/screens/ItemSelectionScreen.kt
@@ -2,14 +2,18 @@ package com.example.rentals.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation3.runtime.NavBackStack
 import com.example.rentals.ui.components.itemselection.ItemSelectionTopBar
 
 @Composable
 fun ItemSelectionScreen() {
-    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-        ItemSelectionTopBar()
+    Scaffold(modifier = Modifier.fillMaxSize(), topBar = { ItemSelectionTopBar() }) {innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {}
     }
 }
