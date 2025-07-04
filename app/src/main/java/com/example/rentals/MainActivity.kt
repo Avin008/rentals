3e50afc27ddeb90bea889894ff161806ed2a9c11
avinash <avinashnanda008@gmail.com>
2025-07-04 23:03:37 +0530

    added: floating button

--- app/src/main/java/com/example/rentals/MainActivity.kt
+++ app/src/main/java/com/example/rentals/MainActivity.kt
@@ -6,12 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.example.rentals.navigation.AppNavigation
 import com.example.rentals.navigation.Home
+import com.example.rentals.navigation.ItemSelection
 import com.example.rentals.ui.components.homescreen.TopNavigationBar
 import com.example.rentals.ui.theme.RentalsTheme
 
@@ -26,6 +31,22 @@ class MainActivity : ComponentActivity() {
                     topBar = {
                         if(backStack.lastIndex == 0) TopNavigationBar()
                     },
+                    floatingActionButton = {
+                        if (backStack.lastIndex == 0){
+                            FloatingActionButton(onClick = {
+                                backStack.add(
+                                    ItemSelection(
+                                        deliveryDate = "",
+                                        pickupDate = "",
+                                        deliveryTime = "",
+                                        pickupTime = ""
+                                    )
+                                )
+                            }) {
+                                Icon(Icons.Filled.Add, contentDescription = "Add")
+                            }
+                        }
+                    }
                 ) { innerPadding ->
                     AppNavigation(modifier = Modifier.padding(innerPadding), backStack = backStack)
                 }
