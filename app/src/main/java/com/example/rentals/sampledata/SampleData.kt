0c75894206866364a197da1208f0341f1f8b8738
avinash <avinashnanda008@gmail.com>
2025-07-06 17:09:34 +0530

    added: sample data

--- app/src/main/java/com/example/rentals/sampledata/SampleData.kt
+++ app/src/main/java/com/example/rentals/sampledata/SampleData.kt
@@ -6,6 +6,7 @@ import java.time.LocalDate
 import java.time.LocalTime
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
+import java.util.UUID
 
 val sampleOrderItems = listOf(
     DeliveryItem("1", "Anil Nag", LocalDate.now().toString(), LocalTime.of(10, 30).toString(), 3450.00, "delivery", "user123", null, null, OffsetDateTime.now(ZoneOffset.UTC).toString(), "Badmal"),
@@ -22,9 +23,9 @@ val sampleOrderItems = listOf(
 
 val sampleItemsData = listOf(RentalItem(
     userId = "user_001",
-    id = "item_001",
+    id = UUID.randomUUID().toString(),
     imageUrl = "https://example.com/images/bike1.jpg",
-    name = "Mountain Bike",
+    name = "Chair",
     category = "Bicycles",
     price = 15.0,
     description = "A sturdy mountain bike suitable for all terrains.",
@@ -32,5 +33,44 @@ val sampleItemsData = listOf(RentalItem(
     createdAt = "2025-07-01T10:00:00Z",
     inStock = 3,
     totalItems = 5
-)
+),
+    RentalItem(
+        userId = "user_001",
+        id = UUID.randomUUID().toString(),
+        imageUrl = "https://example.com/images/bike1.jpg",
+        name = "Table",
+        category = "Bicycles",
+        price = 15.0,
+        description = "A sturdy mountain bike suitable for all terrains.",
+        isAvailable = true,
+        createdAt = "2025-07-01T10:00:00Z",
+        inStock = 3,
+        totalItems = 5
+    ),
+    RentalItem(
+        userId = "user_001",
+        id = UUID.randomUUID().toString(),
+        imageUrl = "https://example.com/images/bike1.jpg",
+        name = "Generator",
+        category = "Bicycles",
+        price = 15.0,
+        description = "A sturdy mountain bike suitable for all terrains.",
+        isAvailable = true,
+        createdAt = "2025-07-01T10:00:00Z",
+        inStock = 3,
+        totalItems = 5
+    ),
+    RentalItem(
+        userId = "user_001",
+        id = UUID.randomUUID().toString(),
+        imageUrl = "https://example.com/images/bike1.jpg",
+        name = "Parda",
+        category = "Bicycles",
+        price = 15.0,
+        description = "A sturdy mountain bike suitable for all terrains.",
+        isAvailable = true,
+        createdAt = "2025-07-01T10:00:00Z",
+        inStock = 3,
+        totalItems = 5
+    )
 )
