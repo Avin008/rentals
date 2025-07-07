6de94c625a700e46e1d247762bee203562affa19
avinash <avinashnanda008@gmail.com>
2025-07-07 19:31:51 +0530

    added: top-search-bar

--- app/src/main/java/com/example/rentals/ui/components/itemselection/ItemSelectionTopbar.kt
+++ app/src/main/java/com/example/rentals/ui/components/itemselection/ItemSelectionTopbar.kt
@@ -1,24 +1,86 @@
 package com.example.rentals.ui.components.itemselection
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.isTraversalGroup
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.traversalIndex
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ItemSelectionTopBar() {
-        TopAppBar(title = {
-            Text(text = "Search")
-        }, navigationIcon = {
-            IconButton(onClick = {
-            }) {
-                Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+fun ItemSearchBar(
+    textFieldState: TextFieldState,
+    onSearch: (String) -> Unit,
+    searchResults: List<String>,
+    modifier: Modifier = Modifier
+) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    Box(
+        modifier
+            .semantics { isTraversalGroup = true }.fillMaxWidth().padding(horizontal = 16.dp)
+    ) {
+        SearchBar(
+            modifier = Modifier
+                .align(Alignment.TopCenter).fillMaxWidth()
+                .semantics { traversalIndex = 0f }
+                ,
+            windowInsets = WindowInsets(top = 10),
+            inputField = {
+                SearchBarDefaults.InputField(
+                    query = textFieldState.text.toString(),
+                    onQueryChange = { textFieldState.edit { replace(0, length, it) } },
+                    onSearch = {
+                        onSearch(textFieldState.text.toString())
+                        expanded = false
+                    },
+                    expanded = expanded,
+                    onExpandedChange = { expanded = it },
+                    placeholder = { Text("Search Items") },
+                    leadingIcon = { IconButton(onClick = {}) { Icon(Icons.Default.ArrowBack, contentDescription = null) } },
+                )
+            },
+            expanded = expanded,
+            onExpandedChange = { expanded = it },
+        ) {
+            Column(Modifier.verticalScroll(rememberScrollState())) {
+                searchResults.forEach { result ->
+                    ListItem(
+                        headlineContent = { Text(result) },
+                        modifier = Modifier
+                            .clickable {
+                                textFieldState.edit { replace(0, length, result) }
+                                expanded = false
+                            }
+                            .fillMaxWidth()
+                    )
+                }
             }
-        }, windowInsets = WindowInsets(top = 0))
+        }
     }
+}
--- app/src/main/java/com/example/rentals/ui/screens/ItemSelectionScreen.kt
+++ app/src/main/java/com/example/rentals/ui/screens/ItemSelectionScreen.kt
@@ -5,29 +5,27 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.rentals.sampledata.sampleItemsData
 import com.example.rentals.ui.components.itemselection.CartSummary
 import com.example.rentals.ui.components.itemselection.FilterSection
-import com.example.rentals.ui.components.itemselection.ItemSelectionTopBar
 import com.example.rentals.ui.components.itemselection.ItemList
+import com.example.rentals.ui.components.itemselection.ItemSearchBar
 import com.example.rentals.ui.viewmodels.CartViewModel
 
 @Composable
 fun ItemSelectionScreen(viewModel: CartViewModel = viewModel()) {
 
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val text = rememberTextFieldState("")
 
-    Scaffold(modifier = Modifier.fillMaxSize(), topBar = { ItemSelectionTopBar() }, bottomBar = {
+    Scaffold(modifier = Modifier.fillMaxSize(), topBar = { ItemSearchBar(textFieldState = text, onSearch = { }, searchResults = listOf()) }, bottomBar = {
         CartSummary(10, onContinueClick = {}, totalPrice = 100.00)
     }) {innerPadding ->
         Column(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
