b850ffa4a9ddf5a1a61bac72453b812f17058e11
avinash <avinashnanda008@gmail.com>
2025-10-06 13:04:20 +0530

    added: profile pic

--- app/src/main/java/com/example/rentals/ui/screens/profile/Profile.kt
+++ app/src/main/java/com/example/rentals/ui/screens/profile/Profile.kt
@@ -1,5 +1,6 @@
 package com.example.rentals.ui.screens.profile
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
@@ -16,12 +17,15 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.example.rentals.R
 
 sealed interface ProfileEvent {
     object NavigateBack : ProfileEvent
@@ -119,12 +123,7 @@ private fun BusinessProfile() {
             shape = CircleShape,
             color = MaterialTheme.colorScheme.primaryContainer
         ) {
-            Icon(
-                imageVector = Icons.Default.Business,
-                contentDescription = null,
-                modifier = Modifier.padding(24.dp),
-                tint = MaterialTheme.colorScheme.onPrimaryContainer
-            )
+            Image(modifier = Modifier.size(30.dp).clip(CircleShape), painter = painterResource(id = R.drawable.profile), contentDescription = null)
         }
         
         Spacer(modifier = Modifier.height(16.dp))
